7be4f5aa760ef9a70305b3fd61e28ccfa26e7e7e
LiGu <ligu92@gmail.com>
2014-04-16 14:53:19 -0700

    Added config file from Internet and wrote tests that fail

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -9,3 +9,45 @@ TEST(NginxConfigParserTest, SimpleConfig) {
 
   EXPECT_TRUE(success);
 }
+
+TEST(NginxConfigStatementTest, ToString) {
+	NginxConfigStatement statement;
+	statement.tokens_.push_back("foo");
+	statement.tokens_.push_back("bar");
+	EXPECT_EQ("foo bar;\n", statement.ToString(0));
+}
+
+class NginxStringConfigTest : public ::testing::Test {
+	protected:
+	bool ParseString(const std::string& config_string) {
+		std::stringstream config_stream(config_string);
+		return parser_.Parse(&config_stream, &out_config_);
+	}
+	NginxConfigParser parser_;
+	NginxConfig out_config_;
+};
+
+TEST_F(NginxStringConfigTest, SimpleConfig) {
+	ASSERT_TRUE(ParseString("foo bar;"));
+	EXPECT_EQ(1, out_config_.statements_.size());
+	EXPECT_EQ(2, out_config_.statements_[0]->tokens_.size());
+}
+
+TEST_F(NginxStringConfigTest, SimpleBrokenConfig) {
+	EXPECT_FALSE(ParseString("foo bar"));
+}
+
+TEST_F(NginxStringConfigTest, NestedConfig) {
+	EXPECT_TRUE(ParseString("foo { bar { baz qux; } }"));
+}
+
+// Everything else was copied and pasted from the class lab
+// This is a test of an example configuration file that I copied from the Nginx wiki
+TEST(NginxConfigParserTest, InternetConfig) {
+  NginxConfigParser parser;
+  NginxConfig out_config;
+
+  bool success = parser.Parse("example_config_internet", &out_config);
+
+  EXPECT_TRUE(success);
+}
